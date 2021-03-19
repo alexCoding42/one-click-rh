1551d9be3b8090048be8a3112c9b73bad5cc62f3
alex <alexandre.cisse.ac@gmail.com>
2021-03-19 12:24:53 -0400

    Refacto and clean the code, add lottie animation image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,20 @@
 import React, { ReactNode } from 'react';
 import { Link as ReachLink } from 'react-router-dom';
 import {
+  Avatar,
   Box,
-  Button,
   Flex,
   HStack,
   IconButton,
   Image,
   Link,
   Stack,
+  Text,
   useDisclosure,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react';
-import {
-  MdMenu as MenuIcon,
-  MdClose as CloseIcon,
-  MdAdd as AddIcon,
-} from 'react-icons/md';
-import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import { MdMenu as MenuIcon, MdClose as CloseIcon } from 'react-icons/md';
 
 const Links = [
   { linkName: 'Home', to: '/' },
@@ -29,9 +27,9 @@ const NavLink = ({ children, to }: { children: ReactNode; to: string }) => (
     px={2}
     py={1}
     rounded={'md'}
-    color='gray.200'
-    _hover={{ textDecoration: 'none', bg: 'gray.200', color: 'black' }}
-    _active={{ textDecoration: 'none', bg: 'gray.200', color: 'black' }}
+    color='black'
+    _hover={{ textDecoration: 'none', bg: 'black', color: 'white' }}
+    _active={{ textDecoration: 'none', bg: 'black', color: 'white' }}
     to={to}
   >
     {children}
@@ -42,7 +40,7 @@ const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box bg='black' px={4} borderBottom='5px solid' borderColor='#FF0080'>
+    <Box bg='white' px={4} borderBottom='1px solid' borderColor='gray.300'>
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
         <IconButton
           size={'md'}
@@ -63,37 +61,16 @@ const Navbar = () => {
             ))}
           </HStack>
         </HStack>
-        <Flex alignItems={'center'}>
-          <Button
-            variant={'solid'}
-            color='gray.200'
-            bg='#FF0080'
-            _hover={{ bg: '#7928CA' }}
-            size={'sm'}
-            mr={4}
-            leftIcon={<AddIcon />}
-          >
-            Add appointment
-          </Button>
-          <ColorModeSwitcher />
-          {/* <Menu>
-            <MenuButton
-              as={Button}
-              rounded={'full'}
-              variant={'link'}
-              cursor={'pointer'}
-            >
-              <Avatar
-                size={'sm'}
-                src={
-                  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                }
-              />
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Sign Out</MenuItem>
-            </MenuList>
-          </Menu> */}
+        <Flex alignItems='center'>
+          <Wrap>
+            <WrapItem>
+              <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+            </WrapItem>
+          </Wrap>
+          <HStack fontSize='xl' marginLeft='20px'>
+            <Text color='red.500'>Click</Text>
+            <Text color='black'> & Kiosque RH</Text>
+          </HStack>
         </Flex>
       </Flex>
 
